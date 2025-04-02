c6e54c337e986c3da9f3f5ffbb4957b2d9f26d1d
shakyaprem <76051889+shakyaprem@users.noreply.github.com>
2025-04-02 21:28:33 +0530

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -23,6 +23,7 @@ export const register = async (req, res) => {
         const token = jwt.sign({id: user._id} , secretKey, { expiresIn: '7d' });
         res.cookie('token', token, {
             httpOnly: true,
+            secure: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ?
             'none' : 'strict',
@@ -61,6 +62,7 @@ export const login = async (req, res) => {
         const token = jwt.sign({id: user._id} , secretKey, { expiresIn: '7d' });
         res.cookie('token', token, {
             httpOnly: true,
+            secure: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ?
             'none' : 'strict',
